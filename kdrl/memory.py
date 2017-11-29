6749127bed1a713265b5b511d610f6bfeb6bd0a9
obsproth <obsproth@users.noreply.github.com>
2017-11-29 12:59:08 +0900

    Add memory

---
+++ kdrl/memory.py
@@ -0,0 +1,38 @@
+import numpy as np
+
+class Memory:
+    def __init__(self, capacity, state_shape, action, continuous_action=False):
+        self.capacity = capacity
+        self._next = 0
+        self.filled = (capacity == self._next)
+        #
+        self.s = np.zeros(((capacity,) + state_shape), dtype=np.float32)
+        if continuous_action:
+            self.a = np.zeros((capacity, nb_actions), dtype=np.float32)
+        else:
+            self.a = np.zeros(capacity, dtype=np.int8)
+        self.ns = np.zeros_like(self.s)
+        self.r = np.zeros(capacity, dtype=np.float32)
+        self.c = np.zeros(capacity, dtype=np.bool)
+
+    def push(self, state, action, next_state, reward, cont):
+        p = self._next
+        self.s[p] = state
+        self.a[p] = action
+        if cont:
+            self.ns[p] = next_state
+        self.r[p] = reward
+        self.c[p] = cont
+        #
+        self._next += 1
+        self._next %= self.capacity
+        if self._next == 0:
+            self.filled = True
+
+    def _get_current_size(self):
+        return self.capacity if self.filled else self._next
+
+    def sample(self, num):
+        assert self._get_current_size() >= num
+        idx = np.random.choice(self._get_current_size(), size=num, replace=False)
+        return self.s[idx], self.a[idx], self.ns[idx], self.r[idx], self.c[idx]
