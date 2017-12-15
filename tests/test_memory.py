eb1e2708060706da5b834946e3d9e15741322602
obsproth <obsproth@users.noreply.github.com>
2017-12-15 15:48:00 +0900

    Fix test_memory

--- tests/test_memory.py
+++ tests/test_memory.py
@@ -6,19 +6,24 @@ class TestSingleActionMemory(TestCase):
     def test_discrete(self):
         capacity = 3
         state_shape = (3, 2)
-        nb_actions = 4
-        memory = SingleActionMemory(capacity, state_shape, nb_actions)
-        s = np.arange(state_shape[0] * state_shape[1]).reshape(state_shape)
-        for i in range(5):
-            memory.push(s + i, i % nb_actions, s + i + 1, i, i != 4)
+        num_actions = 4
+        memory = SingleActionMemory(capacity, state_shape)
+        s = np.arange(state_shape[0] * state_shape[1]).reshape(state_shape) + 10
+        memory.start_episode(s, 0)
+        memory.step(s + 1, 1, 101)      # 0: 0->1
+        memory.step(s + 2, 2, 102)      # 1: 1->2
+        memory.step(s + 3, 3, 103)      # 2: 2->3
+        memory.end_episode(s + 4, 104)  # 0: 3->4
         assert np.array_equal(memory.s[0], s + 3)
-        assert np.array_equal(memory.s[1], s + 4)
-        assert np.array_equal(memory.a, np.array([3, 0, 2]))
+        assert np.array_equal(memory.s[1], s + 1)
+        assert np.array_equal(memory.a, np.array([3, 1, 2]))
         assert np.array_equal(memory.s[0] + 1, memory.ns[0])
         assert np.array_equal(memory.ns[1], s + 2)
         assert np.array_equal(memory.s[2] + 1, memory.ns[2])
-        assert np.array_equal(memory.r, np.array([3, 4, 2]))
-        assert memory.c[0] and not memory.c[1] and memory.c[2]
+        assert np.array_equal(memory.r, np.array([104, 102, 103]))
+        assert not memory.c[0]
+        assert memory.c[1]
+        assert memory.c[2]
         for i in range(capacity):
             for elem in memory.sample(i):
                 assert len(elem) == i
