b2b45fba3a826bcf85e236d66d763d6fbf470036
obsproth <obsproth@users.noreply.github.com>
2017-12-16 11:32:36 +0900

    Add files for PyPI installation

---
+++ kdrl/__init__.py
@@ -0,0 +1,3 @@
+from . import agent
+from . import memory
+from . import policy
---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='KDRL',
+    version='0.0.1',
+    packages=find_packages(exclude=['tests']),
+    author='obsproth',
+    url='https://github.com/obsproth/KDRL',
+    install_requires=['Keras>=2.0.0',
+                      ],
+)
+
