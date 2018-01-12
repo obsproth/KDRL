cfcab9fc8bebf1e3523699408d1354ba5ba51915
obsproth <obsproth@users.noreply.github.com>
2018-01-12 23:44:48 +0900

    Add test_boltzmann

--- tests/test_policy.py
+++ tests/test_policy.py
@@ -14,4 +14,7 @@ class TestPolicy(TestCase):
         assert policy(np.array([-5, -20, -10])) == 0
         policy.eps = 0
         assert 0 <= policy(np.array([5, 20, 10])) < 3
+    def test_boltzmann(self):
+        policy = Boltzmann()
+        assert 0 <= policy(np.array([5, 20, 10])) < 3
 
