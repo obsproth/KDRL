2cdeff914634f1eb85cc49bac0c39aa04213f17f
obsproth <obsproth@users.noreply.github.com>
2018-02-21 21:23:30 +0900

    Version 0.1.0 release

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='KDRL',
-    version='0.0.2',
+    version='0.1.0',
     packages=find_packages(exclude=['tests']),
     author='obsproth',
     url='https://github.com/obsproth/KDRL',
