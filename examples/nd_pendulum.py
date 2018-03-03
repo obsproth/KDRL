3a873c406aaf116bb81bd2129f4a1a932cd803bc
obsproth <obsproth@users.noreply.github.com>
2018-03-03 17:32:30 +0900

    Add NDAgent (WIP)

---
+++ examples/nd_pendulum.py
@@ -0,0 +1,48 @@
+from kdrl.agents.nd import NDAgent
+from kdrl.trainer import GymTrainer
+import numpy as np
+
+from keras.models import Sequential, Model
+from keras.layers import Input, InputLayer, Dense, Lambda, Concatenate
+
+import gym
+
+def get_actor_model(state_shape):
+    return Sequential([InputLayer(input_shape=state_shape),
+                       Dense(64, activation='relu'),
+                       Dense(64, activation='relu'),
+                       Dense(1, activation='tanh'),
+                       Lambda(lambda x: 2 * x)])
+
+def get_critic_model(state_shape):
+    si = Input(state_shape)
+    ai = Input((1,))
+    x = Concatenate()([si, ai])
+    x = Dense(64, activation='relu')(x)
+    x = Dense(64, activation='relu')(x)
+    x = Dense(1)(x)
+    return Model([si, ai], x)
+
+def main():
+    env = gym.make('Pendulum-v0')
+    #
+    state_shape = env.observation_space.shape
+    action_space = [env.action_space.low[0], env.action_space.high[0]]
+    agent = NDAgent(action_space=action_space,
+                    core_actor_model=get_actor_model(state_shape),
+                    core_critic_model=get_critic_model(state_shape),
+                    optimizer='adam',
+                    memory=30000,
+                    target_model_update=5,
+                    )
+    trainer = GymTrainer(env, agent)
+    # training
+    result = trainer.train(200)
+    # test
+    result = trainer.test(5, render=True)
+    for i, reward in enumerate(result['reward']):
+        print('episode {}: {}'.format(i, reward))
+
+if __name__ == '__main__':
+    main()
+
---
+++ kdrl/agents/nd.py
@@ -0,0 +1,105 @@
+import numpy as np
+
+from keras import backend as K
+from keras.models import Model, model_from_json
+from keras.layers import Input, Lambda, dot
+
+from .core import AbstractAgent
+from ..memory import SingleActionMemory
+
+class NDAgent(AbstractAgent):
+    def __init__(self,
+                 action_space,
+                 core_actor_model,
+                 core_critic_model,
+                 optimizer,
+                 memory,
+                 *args,
+                 loss='mean_squared_error',
+                 gamma=0.99,
+                 target_model_update=1,
+                 warmup=100,
+                 batch_size=32,
+                 **kwargs):
+        super().__init__(action_space, *args, **kwargs)
+        self.core_actor_model = core_actor_model
+        self.core_critic_model = core_critic_model
+        #
+        self.optimizer = optimizer
+        self.loss = loss
+        if isinstance(memory, int):
+            self.memory = SingleActionMemory(int(memory), core_actor_model.inputs[0]._keras_shape[1:], continuous_action=True)
+        else:
+            self.memory = memory
+        self.gamma = gamma
+        self.target_model_update = target_model_update
+        self.warmup = warmup
+        self.batch_size = batch_size
+        #
+        # compile
+        i = Input(core_actor_model.inputs[0]._keras_shape[1:])
+        self.combined_model = Model(i, core_critic_model([i, core_actor_model(i)]))
+        if target_model_update == 1:
+            self.target_core_critic_model = self.core_critic_model
+            self.target_combined_model = self.combined_model
+        else:
+            self.target_core_critic_model = model_from_json(self.core_critic_model.to_json())
+            self.target_combined_model = model_from_json(self.combined_model.to_json())
+        self.episode_count = 0
+        self.train_count = 0
+        self.train_history = []
+        #
+        core_critic_model.trainable = False
+        self.combined_model.compile(optimizer=self.optimizer, loss=lambda y_true, y_pred: y_true*y_pred)
+        core_critic_model.trainable = True
+        self.core_critic_model.compile(optimizer=self.optimizer, loss=loss)
+        self._sync_target_model()
+    # primary method
+    def start_episode(self, state):
+        self.memory.start_episode(state)
+        action = self._select_action(state)
+        self.memory.set_action(action)
+        return action
+    def step(self, state, reward):
+        self.memory.step(state, reward)
+        self._train()
+        action = self._select_action(state)
+        self.memory.set_action(action)
+        return action
+    def end_episode(self, state, reward):
+        self.memory.end_episode(state, reward)
+        self._train()
+        self.episode_count += 1
+    # select action
+    def _select_action(self, state):
+        action = self.core_actor_model.predict_on_batch(np.asarray([state]))[0]
+        action = np.random.normal(action, 0.1)
+        action = np.clip(action, *self.action_space)
+        return self.select_best_action(state)
+    def select_best_action(self, state):
+        action = self.core_actor_model.predict_on_batch(np.asarray([state]))[0]
+        action = np.clip(action, *self.action_space)
+        return action
+    # training
+    def _train(self):
+        if self.warmup < self.memory._get_current_size():
+            x, y = self._gen_training_data()
+            history = self.core_critic_model.train_on_batch(x, y)
+            self.train_history.append(history)
+            #
+            self.combined_model.train_on_batch(x[0], np.ones((self.batch_size, 1)))
+            #
+            self.train_count += 1
+            if self.target_model_update > 1 and self.train_count % self.target_model_update == 0:
+                self._sync_target_model()
+    def _gen_training_data(self):
+        states, actions, next_states, rewards, cont_flags = self.memory.sample(self.batch_size)
+        pred_Q = self.target_combined_model.predict_on_batch(next_states)
+        max_Q = np.max(pred_Q, axis=-1)
+        inputs = [states, actions]
+        targets = rewards + cont_flags * self.gamma * max_Q
+        return inputs, targets
+    def _sync_target_model(self):
+        if self.target_model_update != 1:
+            self.target_combined_model.set_weights(self.combined_model.get_weights())
+
