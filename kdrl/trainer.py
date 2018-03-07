5cda1f346eba846310c1228b79044a1a31eac794
obsproth <obsproth@users.noreply.github.com>
2018-03-07 09:53:28 +0900

    Add adjust_render_fps to GymTrainer

--- kdrl/trainer.py
+++ kdrl/trainer.py
@@ -1,8 +1,15 @@
+from time import sleep
+
+
 class GymTrainer:
+
     def __init__(self, env, agent):
         self.env = env
         self.agent = agent
-    def train(self, episode, render=False):
+
+    def train(self, episode, render=False, adjust_render_fps=True):
+        _delay = self._get_delay(adjust_render_fps)
+        #
         env, agent = self.env, self.agent
         result_reward = [0] * episode
         result_steps = [0] * episode
@@ -12,6 +19,7 @@ class GymTrainer:
             while True:
                 if render:
                     env.render()
+                    sleep(_delay)
                 state, reward, done, info = env.step(action)
                 result_reward[i] += reward
                 result_steps[i] += 1
@@ -22,7 +30,9 @@ class GymTrainer:
                     agent.end_episode(state, reward)
                     break
         return {'reward' : result_reward, 'steps' : result_steps}
-    def test(self, episode, render=False):
+
+    def test(self, episode, render=False, adjust_render_fps=True):
+        _delay = self._get_delay(adjust_render_fps)
         env, agent = self.env, self.agent
         result_reward = [0] * episode
         result_steps = [0] * episode
@@ -31,6 +41,7 @@ class GymTrainer:
             while True:
                 if render:
                     env.render()
+                    sleep(_delay)
                 action = agent.select_best_action(state)
                 state, reward, done, info = env.step(action)
                 result_reward[i] += reward
@@ -38,4 +49,12 @@ class GymTrainer:
                 if done:
                     break
         return {'reward' : result_reward, 'steps' : result_steps}
+
+    def _get_delay(self, adjust_render_fps):
+        if type(adjust_render_fps) == int:
+            return 1 / adjust_render_fps
+        elif adjust_render_fps == True:
+            return 1 / self.env.metadata.get('video.frames_per_second', 60)
+        else:
+            return 0
 
